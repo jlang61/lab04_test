2a966b44d3fdb03ea9e8610e1a3be847eb221730
JustinLang3 <>
2022-02-10 06:45:32 +0000

    only intbst.cpp

--- intbst.cpp
+++ intbst.cpp
@@ -78,6 +78,11 @@ void IntBST::printInOrder() const {
     printInOrder(root);
 }
 void IntBST::printInOrder(Node *n) const {
+  if (n){
+    printPreOrder(n->left);
+    cout << n->info << " ";
+    printPreOrder(n->right);
+  }
     // IMPLEMENT HERE
 }
 
@@ -87,6 +92,11 @@ void IntBST::printPostOrder() const {
 }
 
 void IntBST::printPostOrder(Node *n) const {
+  if (n){
+    printPreOrder(n->right);
+    cout << n->info << " ";
+    printPreOrder(n->left);
+  }
     // IMPLEMENT HERE
 }
 
@@ -97,7 +107,12 @@ int IntBST::sum() const {
 
 // recursive helper for sum
 int IntBST::sum(Node *n) const {
-    return 0; // REPLACE THIS NON-SOLUTION
+  int total = 0;
+  if (n){
+    total = total + sum(n->right);
+    total = total + sum(n->left);
+  }
+  return total; // REPLACE THIS NON-SOLUTION
 }
 
 // return count of values
@@ -107,7 +122,12 @@ int IntBST::count() const {
 
 // recursive helper for count
 int IntBST::count(Node *n) const {
-    return 0; // REPLACE THIS NON-SOLUTION
+  int counter = 0;
+  if (n){
+    counter = counter + count(n->right);
+    counter = counter + count(n->left);
+  }
+  return counter; // REPLACE THIS NON-SOLUTION
 }
 
 // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
@@ -117,33 +137,87 @@ int IntBST::count(Node *n) const {
 // Whenever you call this method from somewhere else, pass it
 // the root node as "n"
 IntBST::Node* IntBST::getNodeFor(int value, Node* n) const{
-    return NULL; // REPLACE THIS NON-SOLUTION
+  if(!n) return nullptr;
+  if (this->root->info == value) return this->root;
+  if (getNodeFor(value, n->right)) return getNodeFor(value, n->right);
+  if (getNodeFor(value, n->left)) return getNodeFor(value, n->left);
 }
 
 // returns true if value is in the tree; false if not
 bool IntBST::contains(int value) const {
-    return true; // REPLACE THIS NON-SOLUTION
+  if(!this->root) return false;
+  if (this->root->info == value) return true;
+  IntBST* right = new IntBST;
+  right->root = this->root->right;
+  IntBST* left = new IntBST;
+  left->root = this->root->left;
+  if (left->contains(value) || right->contains(value)) return true;// REPLACE THIS NON-SOLUTION
 }
 
 // returns the Node containing the predecessor of the given value
 IntBST::Node* IntBST::getPredecessorNode(int value) const{
-    return NULL; // REPLACE THIS NON-SOLUTION
+  Node * n = new Node;
+  n = this->root;
+  if(n->left){
+    n = n->left;
+    while(n->right){
+      n = n->right;
+    }
+    return(n);
+  } else{
+    while(n->parent){
+      if(n->parent->info < n->info){
+        return(n->parent);
+      }
+    }
+  }
+  return nullptr;
 }
 
 // returns the predecessor value of the given value or 0 if there is none
 int IntBST::getPredecessor(int value) const{
-    return 0; // REPLACE THIS NON-SOLUTION
+  Node * n = new Node;
+  n = this->root;
+  if(n->left){
+    n = n->left;
+    while(n->right){
+      n = n->right;
+    }
+    return(n->info);
+  } else{
+    while(n->parent){
+      if(n->parent->info < n->info){
+        return(n->parent->info);
+      }
+    }
+  }
+  return 0; // REPLACE THIS NON-SOLUTION
 }
 
 // returns the Node containing the successor of the given value
 IntBST::Node* IntBST::getSuccessorNode(int value) const{
-    return NULL; // REPLACE THIS NON-SOLUTION
+  Node* n = new Node;
+  n = this->root;
+  if (!n) return nullptr;
+  n = n->right;
+  while(n->left){
+    n = n->left;
+  }
+  return n; // REPLACE THIS NON-SOLUTION
 }
 
 // returns the successor value of the given value or 0 if there is none
 int IntBST::getSuccessor(int value) const{
-    return 0; // REPLACE THIS NON-SOLUTION
+  Node* n = new Node;
+  n = this->root;
+  if (!n) return 0;
+  n = n->right;
+  while(n->left){
+    n = n->left;
+  }
+  return n->info; // REPLACE THIS NON-SOLUTION
 }
+
 
 // deletes the Node containing the given value from the tree
 // returns true if the node exist and was deleted or false if the node does not exist
